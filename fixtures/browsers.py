621fbc0906ea8042f4f9f8ea10b793b103418b5d
Michael-Shkryabin <mihail.shkryabin@gmail.com>
2025-04-11 16:37:10 +0300

    Плагины Pytest.

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -2,6 +2,13 @@ import pytest
 from playwright.sync_api import Page, Playwright
 
 
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
+
+
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -31,5 +38,6 @@ def initialize_browser_state(playwright: Playwright):
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
-    page = context.new_page()
-    yield page
+    yield context.new_page()
+
+    browser.close()
