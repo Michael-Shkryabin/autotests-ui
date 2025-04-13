9566306a5635ff2c637cc9d8b642ba4047bc171c
Michael-Shkryabin <mihail.shkryabin@gmail.com>
2025-04-13 18:10:45 +0300

    Перезапуски автотестов в Pytest

--- tests/test_reruns.py
+++ tests/test_reruns.py
@@ -4,12 +4,12 @@ import random
 PLATFORM = 'Windows'
 
 
-@pytest.mark.flaky(reruns=3, rerunns_delay=2)
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
 def test_reruns():
     assert random.choice([True, False])
 
 
-@pytest.mark.flaky(reruns=3, rerunns_delay=2)
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
 class TestReruns:
     def test_rerun_1(self):
         assert random.choice([True, False])
@@ -18,6 +18,6 @@ class TestReruns:
         assert random.choice([True, False])
 
 
-@pytest.mark.flaky(reruns=3, rerunns_delay=2, condition=PLATFORM == "Windows")
+@pytest.mark.flaky(reruns=3, reruns_delay=2, condition=PLATFORM == "Windows")
 def test_rerun_with_conditions():
     assert random.choice([True, False])
