51d67aebed40f72b3c3f1105f7ca248aa74a39be
Michael-Shkryabin <mihail.shkryabin@gmail.com>
2025-04-23 16:26:24 +0300

    Практические применение PageFactory в автотестах

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -11,10 +11,23 @@ class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.dashboard_toolbar = DashboardToolbarViewComponent(page)
-        self.scores_chart_view = ChartViewComponent(page, 'scores', 'scatter')
-        self.courses_chart_view = ChartViewComponent(page, "courses", "pie")
-        self.students_chart_view = ChartViewComponent(page, "students", "bar")
-        self.activities_chart_view = ChartViewComponent(page, 'activities', 'line')
         self.navbar = NavBarComponent(page)
         self.sidebar = SidebarComponent(page)
+        self.dashboard_toolbar = DashboardToolbarViewComponent(page)
+        self.scores_chart = ChartViewComponent(page, 'scores', 'scatter')
+        self.courses_chart = ChartViewComponent(page, "courses", "pie")
+        self.students_chart = ChartViewComponent(page, "students", "bar")
+        self.activities_chart = ChartViewComponent(page, 'activities', 'line')
+        self.dashboard_toolbar_view_component = DashboardToolbarViewComponent(page)
+
+    def check_visible_students_chart(self):
+        self.students_chart.check_visible('Students')
+
+    def check_visible_activities_chart(self):
+        self.activities_chart.check_visible('Activities')
+
+    def check_visible_courses_chart(self):
+        self.courses_chart.check_visible('Courses')
+
+    def check_visible_scores_chart(self):
+        self.scores_chart.check_visible('Scores')
