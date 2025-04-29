376785032a0012715858d18f5a16e1310debaf34
Michael-Shkryabin <mihail.shkryabin@gmail.com>
2025-04-29 15:33:37 +0300

    Наращивание тестовой базы и рефакторинг

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -3,7 +3,8 @@ from playwright.sync_api import Page
 from components.authentication.registration_form_component import RegistrationFormComponent
 from elements.button import Button
 from pages.base_page import BasePage
-
+from elements.link import Link
+import re
 
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
@@ -11,7 +12,12 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
 
+        self.login_link = Link(page, 'registration-page-login-link', 'Login')
         self.registration_button = Button(page,'registration-page-registration-button', 'Registration')
+
+    def login_link(self):
+        self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login"))
 
     def click_registration_button(self):
         self.registration_button.click()
